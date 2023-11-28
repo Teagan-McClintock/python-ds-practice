37ec95d53407204012780e2a83a93cad4b5bbe87
Carl Molina <carlandrew.molina@gmail.com>
2023-11-28 12:18:05 -0800

    Finished up to 28

--- 26_vowel_count.py
+++ 26_vowel_count.py
@@ -3,7 +3,15 @@ def vowel_count(phrase):
 
         >>> vowel_count('rithm school')
         {'i': 1, 'o': 2}
-        
-        >>> vowel_count('HOW ARE YOU? i am great!') 
+
+        >>> vowel_count('HOW ARE YOU? i am great!')
         {'o': 2, 'a': 3, 'e': 2, 'u': 1, 'i': 1}
     """
+    vowels = 'aeiou'
+    phrase = phrase.lower()
+    freq_counter = {}
+    for letter in phrase:
+        if letter in vowels:
+            freq_counter[letter] = freq_counter.get(letter, 0) + 1
+
+    return freq_counter
--- 27_titleize.py
+++ 27_titleize.py
@@ -7,3 +7,11 @@ def titleize(phrase):
         >>> titleize('oNLy cAPITALIZe fIRSt')
         'Only Capitalize First'
     """
+    phrase = phrase.lower()
+    lower_phrase = phrase.split(" ")
+    titled_phrase = []
+    for word in lower_phrase:
+        title = word[0].upper() + word[1::]
+        titled_phrase.append(title)
+
+    return " ".join(titled_phrase)
